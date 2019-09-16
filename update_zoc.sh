62f7388d5732411688cbad73b052760a22104634
SpecTurrican <lueths@gmail.com>
2019-09-16 23:33:39 +0200

    Update update_zoc.sh

--- update_zoc.sh
+++ update_zoc.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-apt-get install jq -y
+apt-get update && apt-get -y upgrade
+apt-get -y install jq >/dev/null
 
 COIN="zeroone"
-UPDATE_URL="https://github.com/zocteam/zeroonecoin/releases/download/v0.12.3.5/"
-FILENAME="zeroonecore-0.12.3.5-x86_64-linux-gnu.tar.gz"
+UPDATE_URL="https://github.com/zocteam/zeroonecoin/releases/download/v0.12.3.6-rc/"
+FILENAME="zeroonecore-0.12.3.6-x86_64-linux-gnu.tar.gz"
 COIN_SERVICE="${COIN}-cli"
 COIN_DEAMON="${COIN}d"
-SOURCE="/root/zeroonecore-0.12.3/bin/"
+SOURCE_CLEAN="/root/zeroonecore-0.12.3/"
+SOURCE="${SOURCE_CLEAN}bin/"
 TARGET="/usr/local/bin/"
 INSTALLED_VERSION="$(${COIN_SERVICE} getinfo | jq .version)"
-UPDATE_VERSION="120305"
+UPDATE_VERSION="120306"
 
 prepair () {
 
-	# apt-get update && apt-get -y upgrade
-	
 	cd /root/
+	rm ${SOURCE_CLEAN} -r >/dev/null
 	wget ${UPDATE_URL}${FILENAME}
-	tar -xzvf $FILENAME && rm ${FILENAME}
+	tar -xzvf ${FILENAME} && rm ${FILENAME}
 
 
 }
@@ -40,7 +41,7 @@ checkrunning () {
 
 	while ! ${COIN_SERVICE} getinfo >/dev/null 2>&1; do
 		sleep 5
-		error=$(${COIN_SERVICE} getinfo 2>&1 | cut -d: -f4 | tr -d "}")
+		error=$(${COIN_SERVICE} getinfo >/dev/null 2>&1 | cut -d: -f4 | tr -d "}")
 		echo " ... ${COIN}.service is on : ${error}"
 		sleep 2
 	done
@@ -60,19 +61,19 @@ ${COIN_SERVICE} stop
 	echo " ... waiting of ${COIN}.service shutdown... please wait!..."
 
 	while ${COIN_SERVICE} getinfo >/dev/null 2>&1; do
-		error=$(${COIN_SERVICE} getinfo 2>&1 | cut -d: -f4 | tr -d "}")
+		error=$(${COIN_SERVICE} getinfo >/dev/null 2>&1 | cut -d: -f4 | tr -d "}")
 		echo " ... ${COIN}.service is on : ${error}"
-		sleep 3
 	done
 
 	echo "${COIN}.service is shutdown !"
-	sleep 20
+	sleep 10
 
 
 }
 
 crontab_off () {
 
+	rm crontab.txt >/dev/null
 	crontab -l >> crontab.txt
 	crontab -r
 	# '#' am Anfang jeder Zeile einfügen
@@ -88,6 +89,7 @@ crontab_on () {
 	# '#' am Anfang jeder Zeile entfernen
 	sed -i 's@^#@@g' crontab.txt
 	crontab -i crontab.txt
+	rm crontab.txt >/dev/null
 
 
 }
@@ -126,13 +128,13 @@ if [ "$INSTALLED_VERSION" -lt "$UPDATE_VERSION" ]
 		checkshutdown
 		update
 		crontab_on
-
-		echo "update finish"
-
-		${COIN_DEAMON}
-
+		# Start
+		${COIN_DEAMON} >/dev/null
 		checkrunning
 		mnsync
+		# Clean Update
+		rm ${SOURCE_CLEAN} -r >/dev/null
+		echo "update finish"
 
 	else
 
